fa85abe28f2d10000eb1e930a6d51a93e021f524
sagor155670 <sagor@braincraftapps.com>
2023-12-12 16:07:58 +0600

    Initial Commit

---
+++ AudioMixer/AudioMixerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioMixerApp.swift
+//  AudioMixer
+//
+//  Created by Saiful Islam Sagor on 12/12/23.
+//
+
+import SwiftUI
+
+@main
+struct AudioMixerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
